abfce779a16e3c9d3e2cb278543058aa5c5f5a97
bains <bains@192.168.0.35>
2020-06-20 17:04:09 -0400

    Added Grandaughter

---
+++ src/demoinheritance1/Grandaughter.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demoinheritance1;
+
+/**
+ *
+ * @author bains
+ */
+public class Grandaughter {
+    
+    private int jas;
+    private int mnn;
+
+    public int getJas() {
+        return jas;
+    }
+
+    public void setJas(int jas) {
+        this.jas = jas;
+    }
+
+    public int getMnn() {
+        return mnn;
+    }
+
+    public void setMnn(int mnn) {
+        this.mnn = mnn;
+    }
+    
+    
+    
+}
